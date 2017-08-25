c8d0578f7e39fc55619cb7831b71d1adb45e7e88
robincollins <racollins1@me.com>
2017-08-25 13:34:18 +0100

    refactor 2

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -11,5 +11,5 @@
 # end
 
 def fizzbuzz(number)
-  number % 15 == 0 ? "FizzBuzz" : number % 5 == 0 ? "Buzz" : number % 3 == 0 ? "Fizz" : n
+  number % 15 == 0 ? "FizzBuzz" : number % 5 == 0 ? "Buzz" : number % 3 == 0 ? "Fizz" : number
 end
