14f2a756bdb3f21ccb21be2ac01358a74b77495f
robincollins <racollins1@me.com>
2017-08-25 13:16:51 +0100

    green and refactor

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,12 @@
 def fizzbuzz(number)
-  'fizz'
+  if(number % 15 == 0)
+    "FizzBuzz"
+  elsif(number % 3 == 0)
+    "Fizz"
+  elsif(number % 5 == 0)
+    "Buzz"
+  end
 end
+
+
+# (1..n).map { |x| x%15==0 ? "FizzBuzz" : x%5==0 ? "Buzz" : x%3==0 ? "Fizz" : x }
